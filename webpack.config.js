ef021966458504725e3a6440e314b9708ad60f81
Roman Melnishin <roman@Romans-MacBook-Pro.local>
2017-12-20 13:55:52 +0200

    clean up final .war, exclude codebase

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,18 @@
 var path = require('path');
 
-const PATHS = {
-	// build: path.join(__dirname, 'target', 'classes', 'META-INF')
-	build: path.join(__dirname, 'target', 'widget')
-};
-
 module.exports = {
-    devtool: 'source-map',
-    entry: [
-        './src/main/webapp/index.js'
-    ],
-    // output: {
-    //     path: path.join(__dirname, 'build'),
-    //     filename: 'bundle.js',
-    //     publicPath: '/static/'
-    // },
-	output: {
-		path: PATHS.build,
-		filename: 'app-bundle.js'
-	},
+    // devtool: 'source-map',
+	entry: './app/index.js',
+    output: {
+        path: path.join(__dirname, 'target', 'widget'),
+        filename: 'bundle.js'
+    },
     module: {
         loaders: [
             {
                 test: /\.js/,
                 loaders: ['babel-loader'],
-                include: path.join(__dirname, 'src')
+                include: path.join(__dirname, 'app')
             },
             {
                 test: /\.css$/,
